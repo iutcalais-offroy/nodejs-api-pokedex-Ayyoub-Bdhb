802f647b5e0ed914db7abe6accd3bd6568304244
Ayyoub Boudahba <161156230+Ayyoub-Bdhb@users.noreply.github.com>
2026-01-30 16:46:28 +0100

    création des decks

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,18 @@
 import bcrypt from "bcryptjs";
-import {readFileSync} from "fs";
-import {join} from "path";
-import {prisma} from "../src/database";
-import {CardModel} from "../src/generated/prisma/models/Card";
-import {PokemonType} from "../src/generated/prisma/enums";
+import { readFileSync } from "fs";
+import { join } from "path";
+import { prisma } from "../src/database";
+import { CardModel } from "../src/generated/prisma/models/Card";
+import { PokemonType } from "../src/generated/prisma/enums";
 
 async function main() {
     console.log("🌱 Starting database seed...");
 
+    await prisma.deckCard.deleteMany();
+    await prisma.deck.deleteMany();
     await prisma.card.deleteMany();
     await prisma.user.deleteMany();
+
 
     const hashedPassword = await bcrypt.hash("password123", 10);
 
@@ -28,8 +31,8 @@ async function main() {
         ],
     });
 
-    const redUser = await prisma.user.findUnique({where: {email: "red@example.com"}});
-    const blueUser = await prisma.user.findUnique({where: {email: "blue@example.com"}});
+    const redUser = await prisma.user.findUnique({ where: { email: "red@example.com" } });
+    const blueUser = await prisma.user.findUnique({ where: { email: "blue@example.com" } });
 
     if (!redUser || !blueUser) {
         throw new Error("Failed to create users");
@@ -56,7 +59,48 @@ async function main() {
         )
     );
 
+   
+
     console.log(`✅ Created ${pokemonData.length} Pokemon cards`);
+
+    console.log("\n🎉 Database seeding completed!");
+
+    // juste tu fais des decks à partir de createdCards que tu combienes avec math.random pour avoir l'aspect aléatoire et le slice qui comme un lim 10 en sql va limiter à 10 valeurs 
+    const redDeckCards = [...createdCards].sort(() => 0.5 - Math.random()).slice(0, 10);
+    const blueDeckCards = [...createdCards].sort(() => 0.5 - Math.random()).slice(0, 10);
+
+    
+    const redDeck = await prisma.deck.create({
+        data: {
+            name: "Starter Deck",
+            userId: redUser.id,
+        },
+    });
+
+    for (const card of redDeckCards) {
+        await prisma.deckCard.create({
+            data: {
+                deckId: redDeck.id,
+                cardId: card.id,
+            },
+        });
+    }
+
+    const blueDeck = await prisma.deck.create({
+        data: {
+            name: "Starter Deck",
+            userId: blueUser.id,
+        },
+    });
+
+    for (const card of blueDeckCards) {
+        await prisma.deckCard.create({
+            data: {
+                deckId: blueDeck.id,
+                cardId: card.id,
+            },
+        });
+    }
 
     console.log("\n🎉 Database seeding completed!");
 }
